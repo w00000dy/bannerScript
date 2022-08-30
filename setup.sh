a15652c8c52d08389f42e865848c79573c9f11ad
WoodyLetsCode <27882680+WoodyLetsCode@users.noreply.github.com>
2022-08-30 14:02:56 +0200

    first commit

---
+++ setup.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+lightCyan='\033[1;36m'
+NC='\033[0m' # No Color
+
+printf "${lightCyan}Installiere neofetch${NC}\n"
+apt install neofetch -y
+
+printf "${lightCyan}Erstelle /etc/update-motd.d/01-neofetch Datei${NC}\n"
+rm -f /etc/update-motd.d/01-neofetch
+cp -v 01-neofetch /etc/update-motd.d/01-neofetch
+chmod 755 /etc/update-motd.d/01-neofetch
+
+printf "${lightCyan}Erstelle /etc/ssh/banner Datei${NC}\n"
+rm -f /etc/ssh/banner
+cp -v banner /etc/ssh/banner
+
+printf "${lightCyan}Erstelle /etc/ssh/neofetch.conf Datei${NC}\n"
+rm -f /etc/ssh/neofetch.conf
+cp -v config.conf /etc/ssh/neofetch.conf
+
+printf "${lightCyan}Passe SSH-Konfiguration (/etc/ssh/sshd_config) an${NC}\n"
+echo 'Banner /etc/ssh/banner' >> /etc/ssh/sshd_config
+
+printf "${lightCyan}Starte SSH Service neu${NC}\n"
+systemctl restart ssh.service

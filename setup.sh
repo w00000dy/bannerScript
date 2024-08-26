a84a35574e218a24aa14d5bbb30111be8594cdd2
Woody <xaver@holzapfel.net>
2024-08-26 14:15:21 +0200

    Switch to fastfetch

--- setup.sh
+++ setup.sh
@@ -3,24 +3,27 @@
 lightCyan='\033[1;36m'
 NC='\033[0m' # No Color
 
-printf "${lightCyan}Installiere neofetch${NC}\n"
-apt install neofetch -y
+printf "${lightCyan}Uninstall neofetch${NC}\n"
+apt remove neofetch -y
 
-printf "${lightCyan}Erstelle /etc/update-motd.d/01-neofetch Datei${NC}\n"
+printf "${lightCyan}Install fastfetch${NC}\n"
+add-apt-repository ppa:zhangsongcui3371/fastfetch -y
+apt install fastfetch -y
+
+printf "${lightCyan}Remove /etc/update-motd.d/01-neofetch file${NC}\n"
 rm -f /etc/update-motd.d/01-neofetch
-cp -v 01-neofetch /etc/update-motd.d/01-neofetch
-chmod 755 /etc/update-motd.d/01-neofetch
 
-printf "${lightCyan}Erstelle /etc/ssh/banner Datei${NC}\n"
+printf "${lightCyan}Create /etc/update-motd.d/01-fastfetch file${NC}\n"
+rm -f /etc/update-motd.d/01-fastfetch
+cp -v 01-neofetch /etc/update-motd.d/01-fastfetch
+chmod 755 /etc/update-motd.d/01-fastfetch
+
+printf "${lightCyan}Create /etc/ssh/banner file${NC}\n"
 rm -f /etc/ssh/banner
 cp -v banner /etc/ssh/banner
 
-printf "${lightCyan}Erstelle /etc/ssh/neofetch.conf Datei${NC}\n"
-rm -f /etc/ssh/neofetch.conf
-cp -v config.conf /etc/ssh/neofetch.conf
-
-printf "${lightCyan}Passe SSH-Konfiguration (/etc/ssh/sshd_config) an${NC}\n"
+printf "${lightCyan}Customize SSH configuration (/etc/ssh/sshd_config)${NC}\n"
 sed -i 's/#Banner none/Banner \/etc\/ssh\/banner/g' /etc/ssh/sshd_config
 
-printf "${lightCyan}Starte SSH Service neu${NC}\n"
+printf "${lightCyan}Restart SSH service${NC}\n"
 systemctl restart ssh.service
